16048830706086f6f0fe50e15e9266f5a7832363
Gagan Bansal <bansalg@cs.washington.edu>
2016-06-04 05:44:23 -0700

    fix unicode dict reader

--- inout/csv_utils.py
+++ inout/csv_utils.py
@@ -1,11 +1,14 @@
 """Module for useful csv related function."""
-import codecs, csv, cStringIO
+import codecs
+import csv
+import cStringIO
 
 
 class UTF8Recoder:
     """
     Iterator that reads an encoded stream and reencodes the input to UTF-8
     """
+
     def __init__(self, f, encoding):
         self.reader = codecs.getreader(encoding)(f)
 
@@ -15,22 +18,13 @@ class UTF8Recoder:
     def next(self):
         return self.reader.next().encode("utf-8")
 
-class UnicodeReader:
-    """
-    A CSV reader which will iterate over lines in the CSV file "f",
-    which is encoded in the given encoding.
-    """
 
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        f = UTF8Recoder(f, encoding)
-        self.reader = csv.reader(f, dialect=dialect, **kwds)
+def UnicodeDictReader(utf8_data, **kwargs):
+    csv_reader = csv.DictReader(utf8_data, **kwargs)
+    for row in csv_reader:
+        yield {key: unicode(value, 'utf-8')
+               for key, value in row.iteritems()}
 
-    def next(self):
-        row = self.reader.next()
-        return [unicode(s, "utf-8") for s in row]
-
-    def __iter__(self):
-        return self
 
 class UnicodeWriter:
     """
